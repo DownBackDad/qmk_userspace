4cf9664338678c689fd5b7472ef7f42c48ebc883
DownBackDad <40786898+DownBackDad@users.noreply.github.com>
2026-01-30 18:43:43 -0700

    Add initial config for Iris downbackdad keymap
    This is the keymap in a functional, but not totally finished,
    state. Main things missing right now are:
    
    - Useful symbol layer
    - Flow tap configuration
    - Majority of magic rules

---
+++ keyboards/keebio/iris/rev8/keymaps/downbackdad/config.h
@@ -0,0 +1,18 @@
+/* config.h - Keebio Iris Rev 8. - downbackdad keymap
+ * 
+ * Contains the preprocessor directives for customizing feature behaviour for my
+ * custom QMK keymap on the Keebio Iris Rev. 8.
+ * 
+ * Author: Kris Nelson
+ * Created: 2026-01-30
+ * URL: https://github.com/DownBackDad/qmk_userspace
+ */
+
+#define CAPS_WORD_INVERT_ON_SHIFT
+#define CHORDAL_HOLD
+#define COMBO_STRICT_TIMER
+#define COMBO_TERM 35
+#define FLOW_TAP_TERM 150
+#define PERMISSIVE_HOLD
+#define SPECULATIVE_HOLD
+#define TAPPING_TERM 250
---
+++ keyboards/keebio/iris/rev8/keymaps/downbackdad/rules.mk
@@ -0,0 +1,13 @@
+# rules.mk - Keebio Iris Rev. 8 - downbackdad keymap
+# 
+# Contains the makefile rules for building my custom QMK keymap on the Keebio
+# Iris Rev. 8.
+# 
+# Author: DownBackDad
+# Created: 2026-01-30
+# URL: https://github.com/DownBackDad/qmk_userspace
+
+CAPS_WORD_ENABLE = yes
+COMBO_ENABLE = yes
+LAYER_LOCK_ENABLE = yes
+REPEAT_KEY_ENABLE = yes
