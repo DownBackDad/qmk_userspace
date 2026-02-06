77dbb7b0009802dfe44f3d8d2e2e6d933271f6b0
DownBackDad <40786898+DownBackDad@users.noreply.github.com>
2026-02-06 12:32:19 -0700

    Disable flow tap on shift home row mods

--- keyboards/keebio/iris/rev8/keymaps/downbackdad/keymap.c
+++ keyboards/keebio/iris/rev8/keymaps/downbackdad/keymap.c
@@ -133,6 +133,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 /******************************************************************************
+ * Behaviour: Flow tap.
+ ******************************************************************************/
+
+uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t* record, 
+                           uint16_t prev_keycode) {
+  if (is_flow_tap_key(keycode) && is_flow_tap_key(prev_keycode)) {
+    switch (keycode) {
+    case HM_LSFS:
+    case HM_RSFH:
+      return 0;  // Disable flow tap on shift home row mods.
+    default:
+      return FLOW_TAP_TERM;
+    }
+  }
+  
+  return 0;  // Disable flow tap.
+}
+
+/******************************************************************************
  * Behaviour: Keycode custom processing.
  ******************************************************************************/
 
